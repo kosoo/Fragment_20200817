9798e1cdd0edde677b7b5db75760e3a12aa06030
이용찬 <lee9890@naver.com>
2020-08-18 19:52:12 +0900

    SecondFragment 추가 /활용  - 이벤트처리 X (토스트 띄울 예정)

---
+++ app/src/main/java/kr/co/tjoeun/fragment_20200817/fragments/SecondFragment.kt
@@ -0,0 +1,29 @@
+package kr.co.tjoeun.fragment_20200817.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_second.*
+import kr.co.tjoeun.fragment_20200817.R
+
+class SecondFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_second, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        toastBtn.setOnClickListener {
+//            Toast.makeText(this, )
+        }
+
+    }
+}
